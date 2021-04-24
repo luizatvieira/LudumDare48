e32c4b54baa170a5103379f2e8237b837bd26d42
luizatvieira <luizatvieira@yahoo.com.br>
2021-04-24 17:42:58 -0300

    Added and scripted fuel slider

---
+++ Assets/Scripts/Fuel/FuelController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelController : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    private float currentFuel;
+    private float usedFuel = 1f;
+    private float maxFuel = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentFuel = maxFuel;
+        InvokeRepeating( "DecFuel", 3f, 0.5f );
+    }
+
+    private void DecFuel()
+    {
+        currentFuel -= usedFuel;
+        slider.value = currentFuel/maxFuel;
+    }
+
+    public void AddFuel( float receivedFuel )
+    {
+        currentFuel += receivedFuel;
+    }
+
+}
